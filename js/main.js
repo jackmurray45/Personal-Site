c799f8e1b01c40731e3e89e7db713d8f0d148744
jackmurray45 <jackmurray445@gmail.com>
2018-02-01 23:41:44 -0800

    made more responsive

--- js/main.js
+++ js/main.js
@@ -7,7 +7,6 @@ $(document).ready(function(){
 
 
 
-
 $(document).ready(function(){
   // Add smooth scrolling to all links
   $(".scroll").on('click', function(event) {
@@ -35,29 +34,29 @@ $(document).ready(function(){
 
 $(function(){
     $(window).scroll(function() { 
-        if ($(this).scrollTop() > 800 && $(".about").css('visibility') == 'hidden') {
+        if ($(this).scrollTop() > 650 && $(".about").css('visibility') == 'hidden') {
         	$(".about").css('visibility','visible').hide();
         	$(".about").css('visibility','visible').fadeIn('slow');    
         }
 
-        if ($(this).scrollTop() > 1800 && $(".projectHeader").css('visibility') == 'hidden') {
+        if ($(this).scrollTop() > 1750 && $(".projectHeader").css('visibility') == 'hidden') {
         	$(".projectHeader").css('visibility','visible').hide();
         	$(".projectHeader").css('visibility','visible').fadeIn('slow');
         	$(".FabFlix").css('visibility','visible').hide();
         	$(".FabFlix").css('visibility','visible').fadeIn('slow');      
         } 
 
-        if ($(this).scrollTop() > 2900 && $(".UpLift").css('visibility') == 'hidden') {
+        if ($(this).scrollTop() > 3150 && $(".UpLift").css('visibility') == 'hidden') {
         	$(".UpLift").css('visibility','visible').hide();
         	$(".UpLift").css('visibility','visible').fadeIn('slow');    
         }
 
-        if ($(this).scrollTop() > 4200 && $(".simpleCooking").css('visibility') == 'hidden') {
+        if ($(this).scrollTop() > 3950 && $(".simpleCooking").css('visibility') == 'hidden') {
         	$(".simpleCooking").css('visibility','visible').hide();
         	$(".simpleCooking").css('visibility','visible').fadeIn('slow');    
         }
 
-        if ($(this).scrollTop() > 5700 && $(".leadership").css('visibility') == 'hidden') {
+        if ($(this).scrollTop() > 5550 && $(".leadership").css('visibility') == 'hidden') {
           $(".leaderHeader").css('visibility','visible').hide();
           $(".leadershipHeader").css('visibility','visible').fadeIn('slow');  
         	$(".leadership").css('visibility','visible').hide();
@@ -65,6 +64,48 @@ $(function(){
         }    
     });
 });
+
+
+
+function divReSize(){
+  if($(window).width() < 1331){
+      $('.aboutText').css({"width":'80%'});
+  }
+  if($(window).width() > 1331){
+      $('.aboutText').css({"width":'40%'});
+  }
+
+   if($(window).width() < 1224){
+      $('.fabFlixBox').css({"width":'80%', "top":"0", "transform":"translateY(0%)"});
+  }
+  if($(window).width() > 1224){
+      $('.fabFlixBox').css({"width":'40%', "top":"50%", "transform":"translateY(-100%)"});
+  }
+
+  if($(window).width() < 1107){
+      $('.upliftbox').insertBefore('.upLiftPhotos');
+      $('.upliftbox').css({"width":'80%'});
+  }
+  if($(window).width() > 1107){
+    $('.upliftbox').insertAfter('.upLiftPhotos');
+    $('.upliftbox').css({"width":'40%'});
+  }
+
+  if($(window).width() < 1424){
+      $('.simpleCookingBox').css({"width":'80%', "top":"50%", "transform":"translateY(-50%)"});
+  }
+  if($(window).width() > 1424){
+      $('.simpleCookingBox').css({"width":'40%', "top":"50%", "transform":"translateY(-100%)"});
+  }
+
+}
+
+$(document).ready(divReSize);
+$(window).resize(divReSize);
+
+
+
+
 
 
 
@@ -79,7 +120,7 @@ $(function(){
     fadeInAndTime(".first", 2250);
     fadeInAndTime(".navbar", 2250);
     fadeInAndTime(".boldWords", 2250);
-    fadeInAndTime(".bold1", 2250);
-    setTimeout(function(){fadeInAndTime(".bold2", 2250);},2000);
-    setTimeout(function(){fadeInAndTime(".bold3", 2250);},4000);
+    setTimeout(function(){fadeInAndTime(".bold1", 2250);},2250);
+    setTimeout(function(){fadeInAndTime(".bold2", 2250);},4000);
+    setTimeout(function(){fadeInAndTime(".bold3", 2250);},5750);
   })});
